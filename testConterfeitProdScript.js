8313cb2f3fd3c382351cd727e9f2e74bd1e7952f
trevornestman <tnestman@lifetechresources.com>
2019-01-14 17:56:44 -0800

    minor change to the test text font family

--- testConterfeitProdScript.js
+++ testConterfeitProdScript.js
@@ -4,7 +4,7 @@ MAIN.style.transition = "padding-top .2s";
 
 var linkToPage = "#";
 var banner =
-  '<div id="conterfeitBanner" style="width: 100%;position:fixed;top:0;"><div id="conterfeitBannerCont" style="overflow:hidden;box-sizing:border-box;height:4px;padding:0 20px;background-color: #7F76BE;display:flex;align-items:center;justify-content:center;color:#fff;transition:height .3s;border:solid 2px #7F76BE;"><div class="bannerText">Buy real and avoid conterfeit</div><a class="bannerButton" style="white-space: nowrap;border:2px solid #fff; background:none;color:#fff;padding:0 30px;margin-left:20px;border-radius:0;font-size:12px;line-height:2em;font-weight:300;cursor:pointer;outline:none !important;text-decoration:none;" href="' +
+  '<div id="conterfeitBanner" style="font-family: \'Lato\', sans-serif;width: 100%;position:fixed;top:0;"><div id="conterfeitBannerCont" style="overflow:hidden;box-sizing:border-box;height:4px;padding:0 20px;background-color: #7F76BE;display:flex;align-items:center;justify-content:center;color:#fff;transition:height .3s;border:solid 2px #7F76BE;"><div class="bannerText">Buy real and avoid conterfeit</div><a class="bannerButton" style="white-space: nowrap;border:2px solid #fff; background:none;color:#fff;padding:0 30px;margin-left:20px;border-radius:0;font-size:12px;line-height:2em;font-weight:300;cursor:pointer;outline:none !important;text-decoration:none;" href="' +
   linkToPage +
   '">LEARN MORE</a></div></div>';
 
